946f02d0ac4c8bdcf5189b861ebd94eab38fdd58
Hannah M <hannahemalm616@gmail.com>
2021-06-26 20:13:43 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,30 @@
+require 'pry'
 class Actor < ActiveRecord::Base
+  has_many :characters
+  has_many :shows, through: :characters
+  
+  def full_name 
+    full_name = "#{first_name} #{last_name}"
+    return full_name
+  end 
+  
+
+  def list_roles
+     #define the name of the character the actor plays through the actor id - 
+     #binding.pry
+     
+    emilia = Actor.new(:first_name => "Emilia", :last_name => "Clarke")
+    khaleesi = Character.new(:name => "Khaleesi")
+    khaleesi.actor = emilia
+    got = Show.new(:name => "Game of Thrones")
+    khaleesi.show = got
+    khaleesi.save
+    
+    # character = Characters.find_by(:actor_id)
+    # shows = Characters.find_by(:show_id)
+    roles = "#{khaleesi.name} - #{got.name}"
+    return roles
+  end 
+  
   
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,22 @@
 class Character < ActiveRecord::Base
+  belongs_to :actor 
+  belongs_to :show
   
+  def say_that_thing_you_say
+    urkel = Character.new(name: "Steve Urkel")
+    urkel.catchphrase = "Did I do that?"
+    urkel.save
+    characterCatchphrase = "#{urkel.name} always says: #{urkel.catchphrase}"
+    return characterCatchphrase
+  end   
+  
+  def build_network
+    malcolm = Character.new(name: "Malcolm Reynolds")
+    showNetwork = malcolm.build_show(name: "Firefly").build_network(:call_letters => "Fox")
+    show = malcolm.show
+    return showNetwork
+    # expect(show.name).to eq("Firefly")
+    # expect(show.network.call_letters).to eq("Fox") 
+    
+  end  
 end
